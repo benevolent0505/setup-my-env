d69fdb00e1442f1a7921e4f9a769cf0c4d313c7c
benevolent0505 <mikio.fjt@gmail.com>
2016-12-17 07:22:05 +0900

    include recipes

--- recipes.rb
+++ recipes.rb
@@ -4,3 +4,6 @@ include_recipe './cookbooks/packages.rb'
 include_recipe './cookbooks/perl.rb'
 include_recipe './cookbooks/ruby.rb'
 include_recipe './cookbooks/node.rb'
+
+include_recipe './cookbooks/dotfiles.rb'
+include_recipe './cookbooks/settings.rb'
