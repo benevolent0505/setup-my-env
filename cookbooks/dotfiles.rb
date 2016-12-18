bb625e4cf4ccad979c96f58a6524a76b39678ff3
benevolent0505 <mikio.fjt@gmail.com>
2016-12-18 22:22:20 +0900

    fix link setting

--- cookbooks/dotfiles.rb
+++ cookbooks/dotfiles.rb
@@ -1,15 +1,18 @@
-git 'develop/src/github.com/benevolent0505/dotfiles' do
-  cwd "/Users/#{node[:user]}"
+git "#{ENV[HOME]}/develop/src/github.com/benevolent0505/dotfiles" do
   repository 'git://github.com/benevolent0505/dotfiles'
 end
 
-link '.emacs.d' do
-  cwd "/Users/#{node[:user]}"
+link "#{ENV[HOME]}/.emacs.d" do
   to 'develop/src/github.com/benevolent0505/dotfiles/.emacs.d'
+  not_if 'test -d .emacs.d'
 end
 
-link '.bashrc' do
-  cwd "/Users/#{node[:user]}"
+link "#{ENV[HOME]}/.bashrc" do
   to 'develop/src/github.com/benevolent0505/dotfiles/config/.bashrc'
-  not_if 'test $(cat .bashrc)'
+  not_if 'test -e .bashrc'
+end
+
+link "#{ENV[HOME]}/.config/fish/config.fish" do
+  to 'develop/src/github.com/benevolent0505/dotfiles/config/config.fish'
+  not_if 'test -e .config/fish/config.fish'
 end
