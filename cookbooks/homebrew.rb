552ecea27c5529dfad29fe557386dcbc011297fa
benevolent0505 <mikio.fjt@gmail.com>
2016-12-18 22:11:11 +0900

    fix homebrew install sequence

--- cookbooks/homebrew.rb
+++ cookbooks/homebrew.rb
@@ -1,10 +1,11 @@
-execute 'Installl command line tools' do
+execute 'Install command line tools' do
   user node[:user]
   command 'xcode-select --install'
+  not_if 'test $(xcode-select -p)'
 end
 
 execute 'Install Homebrew' do
   user node[:user]
-  command 'yes "" | /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"'
+  command 'yes | /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"'
   not_if 'test $(which /usr/local/bin/brew)'
 end
