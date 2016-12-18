c1e45e6fbe08dafcb4e62b83cc1704c1b83349ad
benevolent0505 <mikio.fjt@gmail.com>
2016-12-18 23:47:03 +0900

    fix ENV['HOME']

--- cookbooks/dotfiles.rb
+++ cookbooks/dotfiles.rb
@@ -1,18 +1,18 @@
-git "#{ENV[HOME]}/develop/src/github.com/benevolent0505/dotfiles" do
+git "#{ENV['HOME']}/develop/src/github.com/benevolent0505/dotfiles" do
   repository 'git://github.com/benevolent0505/dotfiles'
 end
 
-link "#{ENV[HOME]}/.emacs.d" do
+link "#{ENV['HOME']}/.emacs.d" do
   to 'develop/src/github.com/benevolent0505/dotfiles/.emacs.d'
   not_if 'test -d .emacs.d'
 end
 
-link "#{ENV[HOME]}/.bashrc" do
+link "#{ENV['HOME']}/.bashrc" do
   to 'develop/src/github.com/benevolent0505/dotfiles/config/.bashrc'
   not_if 'test -e .bashrc'
 end
 
-link "#{ENV[HOME]}/.config/fish/config.fish" do
+link "#{ENV['HOME']}/.config/fish/config.fish" do
   to 'develop/src/github.com/benevolent0505/dotfiles/config/config.fish'
   not_if 'test -e .config/fish/config.fish'
 end
